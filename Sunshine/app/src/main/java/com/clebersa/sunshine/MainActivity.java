000715444dea0e7ddb59c5b92c5553f855622b6b
clebersa <cleber.93cd@gmail.com>
2015-03-24 13:27:07 -0300

    - Finalizando a lição e adicionando detalhes da lição 2.

--- Sunshine/app/src/main/java/com/clebersa/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/clebersa/sunshine/MainActivity.java
@@ -1,17 +1,9 @@
 package com.clebersa.sunshine;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -50,36 +42,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class PlaceholderFragment extends Fragment {
-        ArrayAdapter<String> adapter;
-
-        public PlaceholderFragment(){}
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState){
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ArrayList<String> preview = new ArrayList<>();
-            preview.add("Today - Sunny - 88/63");
-            preview.add("Tomorrow - Foggy- 70/46");
-            preview.add("Weds - Sunny - 72/6363");
-            preview.add("Thurs - Cloudy - 64/51");
-            preview.add("Fri - Rainy - 70/46");
-            preview.add("Sat - Foggy - 76/68");
-            preview.add("Sun - Sunny - 80/50");
-            preview.add("Mons - Sunny - 80/50");
-            preview.add("Tues - Sunny - 80/50");
-
-            adapter = new ArrayAdapter<String>(getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    preview);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapter);
-
-            return rootView;
-        }
-    }
 }
